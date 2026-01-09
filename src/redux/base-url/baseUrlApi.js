17b9d3e28978525948d35b8cb509edac6cc4800c
MdJowelAhmed <mdjowelahmed924@gmail.com>
2026-01-09 11:25:44 +0600

    style: remove bg png add webpp imagefor bg

--- src/redux/base-url/baseUrlApi.js
+++ src/redux/base-url/baseUrlApi.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-export const isProduction = true;
+export const isProduction = false;
 export const baseUrl = isProduction ? "http://72.62.164.122:5000/api/v1" : "http://10.10.7.48:5003/api/v1";
 export const api = createApi({
   reducerPath: "api",
